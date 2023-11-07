3e5c8e54616d9caf33cf18db59de88f55d032b59
Andy Edwards <jedwards@fastmail.com>
2023-11-07 15:09:01 -0600

    feat: add TS defs

---
+++ directed.d.ts
@@ -0,0 +1,6 @@
+declare function findDirectedCycle<N>(
+  startNodes: Iterable<N>,
+  getConnectedNodes: (node: N) => Iterator<N> | Iterable<N> | null | undefined
+): Array<N> | null | undefined;
+
+export = findDirectedCycle;
